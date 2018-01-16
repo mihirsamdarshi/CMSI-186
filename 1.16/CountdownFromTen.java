85a546b94b056eaeb968940af5c5dc547306fa61
Mihir Samdarshi <mihirsamdarshi@yahoo.com>
2018-01-16 13:07:26 -0800

    new commits

--- 1.16/CountdownFromTen.java
+++ 1.16/CountdownFromTen.java
@@ -1,6 +1,6 @@
 public class CountdownFromTen {
     public static void main(String[] args) {
-        for (int i = 10; i >= 0; i++) {
+        for (int i = 10; i >= 0; i--) {
             System.out.print(i + " ");
         }
     }
