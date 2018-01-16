88682ceada7805bdb4deae20f13a6ce454e70e12
Mihir Samdarshi <mihirsamdarshi@yahoo.com>
2018-01-16 10:11:02 -0800

    files for class today

---
+++ 1.16/HelloYouInputValidation.java
@@ -0,0 +1,8 @@
+// CountToTen.java
+public class CountToTen {
+    public static void main(String[] args) {
+         for (int i = 0; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
