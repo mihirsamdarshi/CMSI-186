db8bcbb648944ffe2ce7031d9eae2c1a001a1f64
Mihir Samdarshi <mihirsamdarshi@yahoo.com>
2018-01-19 12:22:21 -0800

    Count Multiples Below working except for last number

---
+++ CountMultiplesBelow.java
@@ -0,0 +1,9 @@
+public class CountMultiplesBelow{
+    public static void main(String[] args) {
+        int multNum = Integer.parseInt(args[0]);
+        int maxCount = Integer.parseInt(args[1]);
+        for(int i = 1; i < maxCount/multNum; i++) {
+            System.out.println(multNum * i);
+        }
+    }
+}
