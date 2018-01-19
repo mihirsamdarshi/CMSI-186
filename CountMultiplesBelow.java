5f20505c2a5e528962c0426934c04e860223abca
Mihir Samdarshi <mihirsamdarshi@yahoo.com>
2018-01-19 12:24:26 -0800

    finished CountMultiplesBelow.java

--- CountMultiplesBelow.java
+++ CountMultiplesBelow.java
@@ -2,7 +2,7 @@ public class CountMultiplesBelow{
     public static void main(String[] args) {
         int multNum = Integer.parseInt(args[0]);
         int maxCount = Integer.parseInt(args[1]);
-        for(int i = 1; i < maxCount/multNum; i++) {
+        for(int i = 1; i <= maxCount/multNum; i++) {
             System.out.println(multNum * i);
         }
     }
