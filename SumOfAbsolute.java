d8bc7a69b21b30654aac5e1e6b9304f5a3433fed
Mihir Samdarshi <mihirsamdarshi@yahoo.com>
2018-01-21 18:47:09 -0800

    finished hw

---
+++ SumOfAbsolute.java
@@ -0,0 +1,23 @@
+public class SumOfAbsolute {
+    public static void main(String[] args) {
+        int sum = 0;
+        int numOfArgs = args.length;
+        int[] parsedNum = new int[args.length];
+        for (int i = 0; i < numOfArgs; i++) {
+            try {
+                parsedNum[i] = Integer.parseInt(args[i]);
+            }
+            catch (NumberFormatException nfe) {
+            };
+        }
+        for (int i = 0; i < numOfArgs; i++) {
+            if (parsedNum[i] < 0) {
+                parsedNum[i] *= -1;
+            }
+            }
+        for(int i = 0; i < numOfArgs; i++) {
+            sum += parsedNum[i];
+        }
+        System.out.println(sum);
+    }
+}
