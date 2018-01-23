76413c6f7ef1364ffff3a8152602b0c23f47e445
Mihir Samdarshi <mihirsamdarshi@yahoo.com>
2018-01-23 13:21:55 -0800

    finished tinkering

--- Basic Java/ReplaceThirdWords.java
+++ Basic Java/ReplaceThirdWords.java
@@ -2,15 +2,12 @@ public class ReplaceThirdWords{
     public static void main(String[] args) {
         int numOfArgs = args.length;
         for(int i = 0; i < numOfArgs; i++) {
-            System.out.print(args[i]);
-            if ((i - 1) % 3 == 0) {
-                System.out.print("");
-            }
-        }
-        for(int i = 0; i < numOfArgs; i++) {
-            System.out.print(args[i] + " ");
+            System.out.println(args[i] + " ");
             if ((i - 1) % 3 == 0) {
                 System.out.print("um ");
+            }
+            if ((i) % 3 == 0) {
+                System.out.print("");
             }
         }
     }
