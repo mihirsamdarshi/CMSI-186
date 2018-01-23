5bdf9f2d15d1d7c293313a5c09a7b33bdf2fe82c
Mihir Samdarshi <mihirsamdarshi@yahoo.com>
2018-01-23 13:38:49 -0800

    wrong file committed, replaced

--- Basic Java/ReplaceThirdWords.java
+++ Basic Java/ReplaceThirdWords.java
@@ -2,12 +2,14 @@ public class ReplaceThirdWords{
     public static void main(String[] args) {
         int numOfArgs = args.length;
         for(int i = 0; i < numOfArgs; i++) {
-            System.out.println(args[i] + " ");
-            if ((i - 1) % 3 == 0) {
-                System.out.print("um ");
+            if ((i-2) % 3 == 0) {
+            System.out.print("");
             }
-            if ((i) % 3 == 0) {
-                System.out.print("");
+            else {
+                System.out.print(args[i] + " ");
+            }
+            if ((i-2) % 3 == 0) {
+            System.out.print("um ");
             }
         }
     }
