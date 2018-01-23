84f58d8d2e8a4f682062983bdc2818793fcdef24
Mihir Samdarshi <mihirsamdarshi@yahoo.com>
2018-01-23 13:20:27 -0800

    deleted .class files

--- Basic Java/ReplaceThirdWords.java
+++ Basic Java/ReplaceThirdWords.java
@@ -2,6 +2,7 @@ public class ReplaceThirdWords{
     public static void main(String[] args) {
         int numOfArgs = args.length;
         for(int i = 0; i < numOfArgs; i++) {
+            System.out.print(args[i]);
             if ((i - 1) % 3 == 0) {
                 System.out.print("");
             }
