d66bf27fde0b4bf3780ddfc51425e7303b70681b
Mihir Samdarshi <mihirsamdarshi@yahoo.com>
2018-02-15 14:09:33 -0800

    constructed risky die

--- SliceNDice/RiskyDie.java
+++ SliceNDice/RiskyDie.java
@@ -7,6 +7,15 @@ public class RiskyDie extends CombatDie {
  * 2 Broken Shield faces
  */
 public RiskyDie() {
-        throw new UnsupportedOperationException();
+        this.faces = new DiceFace[8];
+        faces[0] = new DiceFace(DiceFace.FaceType.SWORD, 2);
+        faces[1] = new DiceFace(DiceFace.FaceType.SWORD, 2);
+        faces[2] = new DiceFace(DiceFace.FaceType.SWORD, 2);
+        faces[3] = new DiceFace(DiceFace.FaceType.SHIELD, 1);
+        faces[4] = new DiceFace(DiceFace.FaceType.SHIELD, 1);
+        faces[5] = new DiceFace(DiceFace.FaceType.HEAL, 1);
+        faces[6] = new DiceFace(DiceFace.FaceType.BROKEN_SHIELD, 1);
+        faces[7] = new DiceFace(DiceFace.FaceType.BROKEN_SHIELD, 1);
+        this.faceUpIndex = 0;
 }
 }
