00c907837d5c0d97dd2c60e3824d57577d1d62cc
Mihir Samdarshi <mihirsamdarshi@yahoo.com>
2018-02-15 14:07:35 -0800

    finished SafeDice

--- SliceNDice/SafeDie.java
+++ SliceNDice/SafeDie.java
@@ -6,6 +6,13 @@ public class SafeDie extends CombatDie {
  * 1 Heal face
  */
 public SafeDie() {
-        throw new UnsupportedOperationException();
+        this.faces = new DiceFace[6];
+        faces[0] = new DiceFace(DiceFace.FaceType.SWORD, 1);
+        faces[1] = new DiceFace(DiceFace.FaceType.HEAL, 1);
+        faces[2] = new DiceFace(DiceFace.FaceType.SHIELD, 1);
+        faces[3] = new DiceFace(DiceFace.FaceType.SHIELD, 1);
+        faces[4] = new DiceFace(DiceFace.FaceType.SWORD, 1);
+        faces[5] = new DiceFace(DiceFace.FaceType.SWORD, 1);
+        this.faceUpIndex = 0;
 }
 }
